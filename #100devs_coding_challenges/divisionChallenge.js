70ac458cd9e1c55c12fae17c31dca29912a11cc5
Memphis83 <kiran.iyer83@gmail.com>
2021-01-12 17:57:59 +0530

    adding coding challenges

---
+++ #100devs_coding_challenges/divisionChallenge.js
@@ -0,0 +1,9 @@
+//create a function that takes in two numbers. 
+//Print to the console the division of the these two numbers rounded down. 
+
+function divideTwoNum(x,y) {
+let result = Math.floor(x / y);
+console.log(result);
+}
+
+divideTwoNum(25,5);
