c4c26c190eaf5077f1ba76a823e2e9e1dc6591ba
Memphis83 <kiran.iyer83@gmail.com>
2021-01-14 12:13:44 +0530

    adding coding challenge day 3

---
+++ #100devs_coding_challenges/sumOfFirstAndLastValue.js
@@ -0,0 +1,13 @@
+//create a function that takes in an array. The function should console.log() the sum of the first value in the array and the last value in the array
+
+const numArray = [10, 20, 30, 40];
+
+
+function dingDong(numArray) {
+	let firstValue = numArray[0]
+  let lastValue = numArray.length -1;
+	let finalValue = firstValue + lastValue;
+	console.log(finalValue);
+}
+
+dingDong(numArray);
