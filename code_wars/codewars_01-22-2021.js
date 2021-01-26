5c60012ae2e9cfc1e63ce8e18751beb778cd0256
Memphis83 <kiran.iyer83@gmail.com>
2021-01-26 17:24:43 +0530

    added 4th codewar challenge

--- code_wars/codewars_01-22-2021.js
+++ code_wars/codewars_01-22-2021.js
@@ -52,6 +52,7 @@ function flip(d,arr) {
   }
  console.log(flip("r",[3, 2, 1, 2]));
 
+// -----------------------------------------------------------------
 
 //*Challenge 3 - Who likes it?
 /* 
@@ -86,3 +87,36 @@ function likes(names) {
 
 
 console.log(likes(["Max", "John", "Mark", "Russell", "Kieran", "Frank", "Dan", "Bob"]));
+
+// -----------------------------------------------------------------
+
+/* Challenge 4  */
+/* 
+Given a month as an integer from 1 to 12,
+return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June),
+is part of the second quarter; and month 11 (November),
+is part of the fourth quarter.
+
+*/
+
+// Solution:
+
+//First Method using less than or equal to
+
+const quarterOf = (month) => {
+  if (month <= 3) {
+    return 1
+  } else if (month <= 6) {
+    return 2
+  } else if (month <= 9) {
+    return 3
+  } else if (month <= 12) {
+    return 4
+  }
+  
+} 
+
+console.log(quarterOf(11))
+
