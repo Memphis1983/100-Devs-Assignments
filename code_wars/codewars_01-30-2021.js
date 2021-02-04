f12aaae6bfece546132d87a14fd0323de0a7a2e3
Memphis83 <kiran.iyer83@gmail.com>
2021-02-05 04:49:35 +0530

    added 5th challenge

--- code_wars/codewars_01-30-2021.js
+++ code_wars/codewars_01-30-2021.js
@@ -135,3 +135,28 @@ return  parseInt(arr.join('').toString(), 2);
 };
 
 console.log(binaryArrayToNumber([0, 0, 0, 1]));
+
+
+/* 
+Challenge 5 - Find the next perfect square!
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is positive.
+
+Examples:
+
+findNextSquare(121) --> returns 144
+findNextSquare(625) --> returns 676
+findNextSquare(114) --> returns -1 since 114 is not a perfect
+*/
+
+  function findNextSquare(sq) {
+    var numb = Math.sqrt(sq);
+    if (numb % 1 != 0) {
+       return -1;
+    }
+
+    return Math.pow(numb + 1, 2);
+  }
+
+  console.log(findNextSquare(625));
