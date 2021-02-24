4906f204f335519b792067b17542e72277de2a9a
Memphis83 <kiran.iyer83@gmail.com>
2021-02-25 00:54:26 +0530

    adding codewars challenge

---
+++ code_wars/codewars_02-25-2021.js
@@ -0,0 +1,10 @@
+//Convert boolean values to strings "yes" or "no"
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  if (bool) {
+    return 'Yes';
+  } else {
+    return 'No';
+  }
+}
