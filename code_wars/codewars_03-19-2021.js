da11275385863e3c02c8cec937568f6b9552ee29
Memphis83 <kiran.iyer83@gmail.com>
2021-03-19 17:48:21 +0530

    adding codewars challenge

---
+++ code_wars/codewars_03-19-2021.js
@@ -0,0 +1,8 @@
+Challenge:  Remove String Spaces
+
+Simple, remove the spaces from the string, then return the resultant string.
+
+Solution: 
+function noSpace(x){
+  return x.split(" ").join("");
+}
