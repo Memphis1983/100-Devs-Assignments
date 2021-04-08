0b2673646f7482bd3f35d41eb9cce9aafd331b9d
Memphis83 <kiran.iyer83@gmail.com>
2021-04-08 15:38:31 +0530

    adding codewars challenge

---
+++ code_wars/codewars_04-08-2021.js
@@ -0,0 +1,18 @@
+Challenge: Price of Mangoes
+
+There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+Examples
+mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
+mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for fre	d
+
+Solution: 
+
+function mango(quantity, price){
+let totalPrice = 0;
+  for(let i = 0; i<=quantity; i++){
+    if (i% 3 !== 0) {
+      totalPrice += price
+      }
+  }
+  return totalPrice
