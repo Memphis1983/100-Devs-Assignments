8bf74cdf17955b8153ca70514d7a48aaa9f4f615
Memphis83 <kiran.iyer83@gmail.com>
2021-04-10 20:59:54 +0530

    adding codewars challenge

---
+++ code_wars/codewars_04-10-2021.js
@@ -0,0 +1,11 @@
+Challenge: MakeUpperCase
+
+Write a function which converts the input string to uppercase.
+
+Solution: 
+
+function makeUpperCase(str) {
+  // Code here
+  const newStr = str.toUpperCase();
+  return newStr;
+}
