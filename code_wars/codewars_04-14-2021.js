3c7abf0b0fbebf47e8f64874f15d79632592c7f0
Memphis83 <kiran.iyer83@gmail.com>
2021-04-14 18:42:03 +0530

    adding codewars challenge

---
+++ code_wars/codewars_04-14-2021.js
@@ -0,0 +1,12 @@
+Challenge: Returning Strings
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+Solution: 
+
+function greet(name){
+  //your code here
+  return `Hello, ${name} how are you doing today?`;
+}
