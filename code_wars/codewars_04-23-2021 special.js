f1a06925eeb499be7dde0167f0fec142c33c9f09
Memphis83 <kiran.iyer83@gmail.com>
2021-04-23 12:42:55 +0530

    adding codewars (special x2) challenge)

---
+++ code_wars/codewars_04-23-2021 special.js
@@ -0,0 +1,36 @@
+Challenge: Template Strings
+
+Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+Task
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+Output
+You must output a string containing the two strings with the word ```' are '```
+
+Solution: 
+
+var TempleStrings = function(obj, feature) {
+  return `${obj} are ${feature}`
+  
+}
+--------------------------------------------
+
+Challenge: Is there a vowel in there?
+
+Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+If they are, change the array value to a string of that vowel.
+
+Return the resulting array.
+
+Solution: 
+
+const vowels = new Set('aeiou');
+
+function isVow(a) {
+  return a.map(value => {
+    let charCode = String.fromCharCode(value);
+    return vowels.has(charCode) ? charCode : value;
+  });
+}
