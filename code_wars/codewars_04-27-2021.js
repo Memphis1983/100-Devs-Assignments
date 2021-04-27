68f635d2d5dea5ebeddba03c0d554bad74372939
Memphis83 <kiran.iyer83@gmail.com>
2021-04-27 10:58:36 +0530

    adding codewars challenge

---
+++ code_wars/codewars_04-27-2021.js
@@ -0,0 +1,18 @@
+Challenge: Convert a string to an array
+
+Write a function to split a string and convert it into an array of words. For example:
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==>["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+Solution: 
+
+function stringToArray(string){
+
+	// code code code
+  
+  const splitArr = string.split(" ");
+  return splitArr;
+
+}
