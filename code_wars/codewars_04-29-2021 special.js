5238232e443e6dd5ec1d912b213af0b2b0f5c918
Memphis83 <kiran.iyer83@gmail.com>
2021-04-29 18:17:28 +0530

    adding codewars (special) challenge

---
+++ code_wars/codewars_04-29-2021 special.js
@@ -0,0 +1,16 @@
+Challenge: Convert number to reversed array of digits
+
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+
+
+Solution: 
+
+function digitize(n) {
+let arr = String(n).split("").reverse().map(Number)
+return arr;
+
+}
