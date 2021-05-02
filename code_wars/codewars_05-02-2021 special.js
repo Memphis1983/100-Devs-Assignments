ece6c9976a2362432c5169019c7bc24093d526b7
Memphis83 <kiran.iyer83@gmail.com>
2021-05-02 15:41:51 +0530

    adding codewars (special) challenge

---
+++ code_wars/codewars_05-02-2021 special.js
@@ -0,0 +1,27 @@
+Challenge: I love you, a little , a lot, passionately ... not at all
+
+Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+I love you
+a little
+a lot
+passionately
+madly
+not at all
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+Solution: 
+
+function howMuchILoveYou(nbPetals) {
+    var petalMap = {
+      0: "not at all",
+      1: "I love you",
+      2: "a little",
+      3: "a lot",
+      4: "passionately",
+      5: "madly"
+    };
+    return petalMap[nbPetals % 6];
+}
