c61fc1f67c4ed49c3acf82fb8606d7e91df9af29
Memphis83 <kiran.iyer83@gmail.com>
2021-05-05 21:26:06 +0530

    adding codewars challenge

---
+++ code_wars/codewars_05-05-2021.js
@@ -0,0 +1,19 @@
+Challenge: UEFA EURO 2016
+
+Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw
+
+
+Solution: 
+
+function uefaEuro2016(teams, scores){
+  const winner = scores[0] > scores[1] ? teams[0] : (scores[0] === scores[1] ? null : teams[1]);
+  const result = `At match ${teams[0]} - ${teams[1]}`;
+  if( winner ) {
+    return `${result}, ${winner} won!`;
+  }
+  return `${result}, teams played draw.`
+}
