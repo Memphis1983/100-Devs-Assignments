9727934c2030102482ccc9dd69841e9142f92e67
Memphis83 <kiran.iyer83@gmail.com>
2021-05-21 15:26:04 +0530

    adding codewars (strings) challenge

---
+++ code_wars/codewars_05-21-2021 strings.js
@@ -0,0 +1,17 @@
+Challenge: Convert a Number to a String!
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999"
+
+Solution: 
+
+function numberToString(num) {
+  // Return a string of the number here!
+  
+  return num.toString();
+}
