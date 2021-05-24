a1b2d481aa542b0b1d26d5eee077c49d3cc7a17e
Memphis83 <kiran.iyer83@gmail.com>
2021-05-24 15:41:16 +0530

    adding codewars (strings) challenge

---
+++ code_wars/codewars_05-24-2021 strings.js
@@ -0,0 +1,32 @@
+Challenge: Dollars and Cents
+
+The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+
+39.99 becomes $39.99
+
+The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
+
+Examples:
+
+3 needs to become $3.00
+
+3.1 needs to become $3.10
+Good luck! Your team knows they can count on you!
+
+Solution: 
+
+function formatMoney(amount){
+  const usd = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    useGrouping: false
+  });
+  return usd.format(amount);
+}
+
+Solution 2: OneLiner
+
+function formatMoney(amount){
+  return '$' + amount.toFixed(2);
+}
