fe52761b232f83b807664a509450f39ceaf9cf06
Memphis83 <kiran.iyer83@gmail.com>
2021-06-02 22:59:37 +0530

    adding codewars (strings) challenge

---
+++ code_wars/codewars_06-02-2021 strings.js
@@ -0,0 +1,31 @@
+Challenge - Sentence Smash
+
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+Example
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+Solution : 
+
+// Smash Words
+function smash (words) {
+    "use strict";
+  return words.join(" ")
+    
+    
+};
+
+Solution 2: 
+
+// Smash Words
+function smash (words) {
+    "use strict";
+    var smashed = '';
+    for(var i = 0; i<words.length; i++) {
+      smashed += words[i];
+      if(i!=words.length-1) {
+        smashed += ' ';
+      }
+    }
+    return smashed;
+};
