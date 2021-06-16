bf53967b9fabde4de1a975d8a6d7b7b7d1ec6243
Memphis83 <kiran.iyer83@gmail.com>
2021-06-16 12:47:23 +0530

    adding codewars (strings) challenge

---
+++ code_wars/codewars_06-16-2021 strings.js
@@ -0,0 +1,21 @@
+Challenge - Reversing Words in a String
+
+You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+Example (Input --> Output)
+
+"Hello World" --> "World Hello"
+"Hi There." --> "There. Hi"
+Happy coding!
+
+
+Solution : 
+function reverse(string){
+    var s = string;
+    s = s.split(' ');
+    s = s.reverse();
+    s = s.join(' ');
+    return s;
+}
